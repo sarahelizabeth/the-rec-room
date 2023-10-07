40c74b28a865a20a790cfb837fdb7e047d5596d8
Sarah <semurray3@gmail.com>
2023-10-06 22:16:52 -0500

    update media settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -185,8 +185,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 # STATIC_URL = 'static/'
-MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # if this isn't working, try this instead:
 STATIC_URL = "/static/"
