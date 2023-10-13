dbbffc68fbd095d58140b70e3cb3edff950ed71f
Sarah <semurray3@gmail.com>
2023-10-13 07:23:41 -0500

    static

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -193,8 +193,8 @@ if USE_SPACES:
     AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
     # AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
-    # AWS_STORAGE_BUCKET_NAME = 'mediabucket'
-    AWS_STORAGE_BUCKET_NAME = 'rec-room-media'
+    AWS_STORAGE_BUCKET_NAME = 'mediabucket'
+    # AWS_STORAGE_BUCKET_NAME = 'rec-room-media'
     AWS_DEFAULT_ACL = 'public-read'
     AWS_S3_ENDPOINT_URL = 'https://rec-room-media.nyc3.digitaloceanspaces.com'
     AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
@@ -202,21 +202,21 @@ if USE_SPACES:
     AWS_LOCATION = 'static'
     STATIC_URL = f'https://{AWS_S3_ENDPOINT_URL}/{AWS_LOCATION}/'
     # DEPRECATED:
-    # STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     # media settings
     AWS_MEDIA_LOCATION = 'media'
     PUBLIC_MEDIA_LOCATION = 'media'
     MEDIA_URL = f'https://{AWS_S3_ENDPOINT_URL}/{AWS_MEDIA_LOCATION}/'
     # DEPRECATED:
-    # DEFAULT_FILE_STORAGE = 'backend.storage_backend.MediaStorage'
-    STORAGES = {
-        'staticfiles': {
-            'BACKEND': 'storages.backends.s3boto3.S3Boto3Storage',
-        },
-        'default': {
-            'BACKEND': 'backend.storage_backend.MediaStorage',
-        },
-    }
+    DEFAULT_FILE_STORAGE = 'backend.storage_backend.MediaStorage'
+    # STORAGES = {
+    #     'staticfiles': {
+    #         'BACKEND': 'storages.backends.s3boto3.S3Boto3Storage',
+    #     },
+    #     'default': {
+    #         'BACKEND': 'backend.storage_backend.MediaStorage',
+    #     },
+    # }
 else:
     STATIC_URL = '/static/'
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
@@ -225,6 +225,7 @@ else:
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'workspace/backend/static'),
 )
 MEDIAFILES_DIRS = (
     os.path.join(BASE_DIR, 'media')
